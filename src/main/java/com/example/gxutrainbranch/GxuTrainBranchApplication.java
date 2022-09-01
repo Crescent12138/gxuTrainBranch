f948f7d952c53c41913b4ed81f8dee280c26fdc4
Crescent12138 <75103155+Crescent12138@users.noreply.github.com>
2022-09-01 16:34:43 +0800

    简单测试了几个接口，修正了关于activity的bug

--- src/main/java/com/example/gxutrainbranch/GxuTrainBranchApplication.java
+++ src/main/java/com/example/gxutrainbranch/GxuTrainBranchApplication.java
@@ -2,8 +2,9 @@ package com.example.gxutrainbranch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class })
 public class GxuTrainBranchApplication {
 
     public static void main(String[] args) {
